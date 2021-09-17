f07d7ecebda54017e6d44ce2c96a0132eeda9dd8
Robert <robert.leffmann@student.kyh.se>
2021-09-17 15:07:20 +0200

    Lägg till övning 1

--- src/kyh/övningar/Main.java
+++ src/kyh/övningar/Main.java
@@ -3,10 +3,28 @@ package kyh.övningar;
 //  https://www.w3resource.com/java-exercises/numbers/index.php
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
 
   public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    //  övning 1, kolla om ett nummer är "fult"
+
+    while (true) {
+      System.out.print("Skriv in ett heltal, 0 för att avsluta: ");
+      int n = in.nextInt();
+
+      if (n <= 0)  break;
+      while (n%2 == 0)  n /= 2;
+      while (n%3 == 0)  n /= 3;
+      while (n%5 == 0)  n /= 5;
+
+      System.out.println(n <= 1 ? "Fult." : "Ej fult.");
+    }
+
+
 
   }
 }
