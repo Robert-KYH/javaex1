45343fd728dd18d3cb65c91024d045b79f81bed1
Robert <robert.leffmann@student.kyh.se>
2021-09-17 14:42:51 +0200

    Nytt projekt

---
+++ src/kyh/övningar/Main.java
@@ -0,0 +1,12 @@
+package kyh.övningar;
+
+//  https://www.w3resource.com/java-exercises/numbers/index.php
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
