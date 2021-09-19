0d06c11b781c3ca58796eee7ff11240560ff29c1
Robert <robert.leffmann@student.kyh.se>
2021-09-19 21:24:08 +0200

    Omfaktorering

--- src/kyh/övningar/Main.java
+++ src/kyh/övningar/Main.java
@@ -1,30 +1,34 @@
-package kyh.övningar;
-
 //  https://www.w3resource.com/java-exercises/numbers/index.php
+
+package kyh.övningar;
 
 import java.util.Scanner;
 import java.lang.Math;
 
+
 public class Main {
 
+  public static Scanner in;
+
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+    in = new Scanner(System.in);
 
-    //  övning 1, kolla om ett nummer är "fult"
+    övn1();
+  }
 
+
+  //  övning 1, kolla om ett tal är "fult"
+  public static void övn1() {
     while (true) {
       System.out.print("Skriv in ett heltal, 0 för att avsluta: ");
       int n = in.nextInt();
 
       if (n <= 0)  break;
-      while (n%2 == 0)  n /= 2;
-      while (n%3 == 0)  n /= 3;
-      while (n%5 == 0)  n /= 5;
+      while (n % 2 == 0)  n /= 2;
+      while (n % 3 == 0)  n /= 3;
+      while (n % 5 == 0)  n /= 5;
 
       System.out.println(n <= 1 ? "Fult." : "Ej fult.");
     }
-
-
-
   }
 }
